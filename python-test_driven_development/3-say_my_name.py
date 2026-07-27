9f4b50eb5876ee458d2f6e7ff5576fc4c374fe01
Luis Felipe Gonzalez <lgonzalez05@gmail.com>
2026-07-26 23:24:55 -0400

    Fix PEP8 E501 line length issue in 3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,7 +3,9 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints 'My name is <first_name> <last_name>' after validating string types."""
+    """Prints 'My name is <first_name> <last_name>'
+    after validating string types.
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
