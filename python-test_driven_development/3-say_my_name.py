b9da5b24b88814d64bbb9161642ffea922ee2178
Luis Felipe Gonzalez <lgonzalez05@gmail.com>
2026-07-26 23:22:32 -0400

    Upload Task 2: Say my name. Write a function that prints My name is <first name> <last name>

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that provides a function for printing a formatted name."""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints 'My name is <first_name> <last_name>' after validating string types."""
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
