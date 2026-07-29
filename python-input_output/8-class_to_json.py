9ecdf0a20310ecf88955bb08886c87649f3413a6
Luis Felipe Gonzalez <lgonzalez05@gmail.com>
2026-07-29 13:36:07 -0400

    Fix PEP8 line length error in Task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,5 +3,5 @@
 
 
 def class_to_json(obj):
-    """Returns the dictionary description for JSON serialization of an object."""
+    """Returns the dictionary description with simple data structure."""
     return obj.__dict__
