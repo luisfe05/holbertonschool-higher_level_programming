2e58d8bba72ef4d6f17a1afae01ecbe2020896a7
Luis Felipe Gonzalez <lgonzalez05@gmail.com>
2026-07-29 11:32:18 -0400

    Upload Task 6: Improve Geometry. Implement area method in BaseGeometry class.

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines the BaseGeometry class with an area method placeholder.
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class for geometric shapes.
+    """
+
+    def area(self):
+        """
+        Raises an Exception indicating that area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
