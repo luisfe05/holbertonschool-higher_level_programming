b08e1ffa39a6e4e49729e382d0b5680e1839e6ec
Luis Felipe Gonzalez <lgonzalez05@gmail.com>
2026-07-29 12:10:18 -0400

    Implement Task 11: Square class with updated __str__ method

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module defining the Square class inheriting from Rectangle.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Representation of a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a new Square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """
+        Return the string representation of the square.
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
