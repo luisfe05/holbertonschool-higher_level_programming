3d3d4c33253271bc7281e69b0c71f504a82b18e3
Luis Felipe Gonzalez <lgonzalez05@gmail.com>
2026-07-29 11:08:06 -0400

    Fix module docstring for Task 0: lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""
+This module provides a function to inspect object attributes and methods.
+"""
+
 
 def lookup(obj):
     """
-    Returns a list of available attributes and methods of an object.
+    Returns the list of available attributes and methods of an object.
     """
     return dir(obj)
